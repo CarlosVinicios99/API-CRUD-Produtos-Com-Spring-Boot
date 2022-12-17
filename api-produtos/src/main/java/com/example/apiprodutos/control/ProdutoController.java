98526668b236bce9ed3b51a8f22d4b06fb58143c
carlosvinicios <carlosvinicios@id.uff.br>
2022-12-16 21:37:57 -0300

    Classe produto finalizada

--- api-produtos/src/main/java/com/example/apiprodutos/control/ProdutoController.java
+++ api-produtos/src/main/java/com/example/apiprodutos/control/ProdutoController.java
@@ -1,5 +1,16 @@
 package com.example.apiprodutos.control;
 
-public class ProdutoController {
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+	
+	@GetMapping(path = "/ola")
+	public String ola() {
+		return "ola";
+	}
+	
 }
--- api-produtos/src/main/java/com/example/apiprodutos/model/entities/Produto.java
+++ api-produtos/src/main/java/com/example/apiprodutos/model/entities/Produto.java
@@ -1,5 +1,36 @@
 package com.example.apiprodutos.model.entities;
 
-public class Produto {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotBlank
+	private String nome;
+	
+	@Min(0)
+	private double preco;
+	
+	
+	public Produto() {
+		
+	}
+	
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	
+	
+	
 }
